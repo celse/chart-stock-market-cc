639fddb91d615413dad5b7f873a5b98f0fee86c0
Obou Brissy <Obou.Brissy@marionegri.it>
2017-10-12 15:51:07 +0200

    ipdate port for heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(flash());
 
 routes(app);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT|| 5000;
 app.listen(port, function(){
     console.log('app Node.js listening on port ' + port + '...');
 })
